c2b73b130910f4610bff75e10491d899b9200fc0
Shane D Wilson <me@shanedwilson.com>
2019-04-30 19:26:04 -0500

    wip

---
+++ SwordAndFather/Data/TargetRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Data.SqlClient;
+using Dapper;
+using SwordAndFather.Models;
+
+namespace SwordAndFather.Data
+{
+    public class TargetRepository
+    {
+        const string ConnectionString = "Server = localhost; Database = SwordAndFather; Trusted_Connection = True;";
+
+        public Target AddTarget(string name, string location, FitnessLevel fitnessLevel, int userId)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var insertQuery = @"
+                       INSERT INTO [dbo].[Targets]
+                               ([Location]
+                               ,[Name]
+                               ,[FitnessLevel]
+                               ,[UserId])
+                    output inserted.*
+                         VALUES
+                               (@location
+                               ,@name
+                               ,@fitnessLevel
+                               ,@userId)";
+
+                var parameters = new
+                {
+                    Name = name,
+                    Location = location,
+                    FitnessLevel = fitnessLevel,
+                    UserId = userId
+                };
+
+                var newTarget = db.QueryFirstOrDefault<Target>(insertQuery, parameters);
+
+                if (newTarget != null)
+                {
+                    return newTarget;
+                }
+                throw new Exception("No target created");
+            }
+        }
+    }
+}
--- SwordAndFather/Models/User.cs
+++ SwordAndFather/Models/User.cs
@@ -7,15 +7,8 @@ namespace SwordAndFather.Models
 {
     public class User
     {
-        public User(string username, string password)
-        {
-            UserName = username;
-            Password = password;
-        }
-
         public int Id { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
-
     }
 }
